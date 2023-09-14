c4e55ffb4aba8a3ff0b62f493dbeb43abcfa36ae
ArnavWakekar <arnav.vip2@gmail.com>
2023-09-15 00:49:04 +0530

    Ddesktop 1st commit

---
+++ ESS_tracker/ESS_tracker/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+public class Class1
+{
+    public static void Main(string[] args)
+    {
+        DesiredCapabilities appCapabilities = new DesiredCapabilities();
+        appCapabilities.SetCapability("app", "C:\\Windows\\system32\\notepad.exe");
+
+        WindowsDriver<WindowsElement> driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+
+        try
+        {
+            driver.Manage().Window.Maximize();
+            // Add your automation code here
+            WindowsElement element = driver.FindElementByName("ButtonName");
+            element.Click();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+}
+
